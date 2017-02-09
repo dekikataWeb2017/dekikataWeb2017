87c547feb6ab72831d808eb7cae3d1fc0bf8e87b
kitagawa <kitagawa@mkpc.local>
2017-02-09 16:16:19 +0900

    update

--- js/jquery.menu.js
+++ js/jquery.menu.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
 
-  $('.contents').hide();
-  $('.contents:first').show();
-  $('.menu li:first').addClass('active');
+  // $('.contents').hide();
+  // $('.contents:first').show();
+  // $('.menu li:first').addClass('active');
 
   $('.menu li').click(function() {
 
-    $('.menu li').removeClass('active');
-    $(this).toggleClass('active');
+    // $('.menu li').removeClass('active');
+    // $(this).toggleClass('active');
     $('.contents').hide();
     $('header').removeClass('open');
+    if($(this).find('a').attr('href') == '#page1'){
+      $('#wrapper').removeClass('back').animate({
+        //backgroundColor: "rgba(255, 255, 255, 0)",
+              color: "#fff",
+          }, 500);
 
+      }else{
+        $('#wrapper').addClass('back').animate({
+                //backgroundColor: "rgba(255, 255, 255, 1)",
+                color: "#3f382e",
+            }, 500);
+      }
     $($(this).find('a').attr('href')).fadeIn();
     return false;
 
